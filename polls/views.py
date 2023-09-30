ac87e6fd2752466f2e7fa96bdbbfb2918ebcca19
abdurrahmanff <abdurrahmanff15@gmail.com>
2023-10-01 01:41:34 +0700

    feat: prevent race conditoin

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.db.models import F
 
 from .models import Question, Choice
 
@@ -36,7 +37,7 @@ def vote(request, question_id):
             },
         )
     else:
-        selected_choice.votes += 1
+        selected_choice.votes = F("votes") + 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
